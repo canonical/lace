2eabd6cb855adb2d8a29a00c019569743beaa7af
Mate Kukri <mate.kukri@canonical.com>
2025-11-17 12:31:16 +0000

    Add VM run script for stubble

---
+++ vm/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+cd $(dirname $0)
+
+pushd ..
+cargo build -p lace-stubble --target x86_64-unknown-uefi
+popd
+
+objcopy \
+    ../target/x86_64-unknown-uefi/debug/lace-stubble.efi stubble.efi
+
+mcopy \
+	-o \
+	-i hdd.img@@1048576 \
+	stubble.efi \
+	::/EFI/BOOT/BOOTX64.EFI
+
+qemu-system-x86_64 \
+	-M q35,accel=kvm \
+	-m 1G \
+	-nographic \
+	-drive if=pflash,unit=0,format=raw,file=code.fd,readonly=on \
+	-drive if=pflash,unit=1,format=raw,file=vars.fd \
+	-drive if=virtio,format=raw,file=hdd.img
