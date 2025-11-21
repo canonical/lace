372b588aa67b51400905d0f377c8945ebf178119
Julian Andres Klode <julian.klode@canonical.com>
2025-11-21 14:45:17 +0100

    all: Dual license our code as GPL-2.0-only or GPL-3.0-only
    Be aware to not copy GPL-2.0-only code over to the crates.

--- lace-platform/src/efi.rs
+++ lace-platform/src/efi.rs
@@ -1,3 +1,3 @@
-// SPDX-License-Identifier: GPL-2.0-only
+// SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only
 // Copyright (C) 2025, Canonical Ltd.
 // Authors: Mate Kukri <mate.kukri@canonical.com>
